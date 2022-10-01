89dae5fd6f2759e6417ca5d174e579fcaa6d4093
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 <ayman20191700160@cis.asu.edu.eg>
2022-10-01 07:12:10 +0200

    added firebase to the website

--- Backend/NaSa/app.py
+++ Backend/NaSa/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, render_template_string,request
+from flask import Flask, render_template, render_template_string, request
 import joblib
 import pandas as pd
 from datetime import datetime
@@ -7,35 +7,51 @@ import pandas as pd
 import warnings
 import joblib
 from datetime import datetime
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import time
+cred = credentials.Certificate("Backend/NaSa/privatekey.json")
+firebase_admin.initialize_app(cred)
 warnings.filterwarnings("ignore")
 
 
 app = Flask(__name__)
 
 
-@app.route('/',methods=['GET'])
+@app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
 
 
+searchKeyword = 'null'
+Documents = {
+    'key1': {'name': 'value_1'},
+    'key2': {'name': 'value_2'}
+}
 
 
-searchKeyword='null'
-Documents = { 
-          'key1': {'name': 'value_1'},
-          'key2': {'name': 'value_2'}
-           }
-@app.route('/predict',methods=['GET'])
+@app.route('/predict', methods=['GET'])
 def SearchResult():
-    searchKeyword=request.args.get('searchkeyword')
-    #use this searchKeyword in your models
-    #Write your code here
-    #pass one or more Dictionary as i did in this function in the next line
-    return render_template('index.html',Documents=Documents)
-    
-if __name__=='__main__':
+    searchKeyword = request.args.get('searchkeyword')
+    # use this searchKeyword in your models
+    # Write your code here
+    db = firestore.client()
+    doc_id = db.collection('Search').document().id
+    doc_ref = db.collection(u'search').document(doc_id).set({
+        u'searchKeyword': searchKeyword,
+        u'result': None
+    })
+
+    doc = db.collection(u'search').document(doc_id).get().to_dict()
+    while doc['result'] == None:
+        time.sleep(5)
+        doc = db.collection(u'search').document(doc_id).get().to_dict()
+    else:
+        return render_template('index.html', Documents=doc)
+
+    # pass one or more Dictionary as i did in this function in the next line
+
+
+if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
